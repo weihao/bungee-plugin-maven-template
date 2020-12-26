daf1c16b154ebdf63dad825a45441fa857f2e717
Weihao <weihao.gao@outlook.com>
2020-12-26 01:03:12 -0500

    Added configuration file

--- src/main/java/org/akadia/template/Main.java
+++ src/main/java/org/akadia/template/Main.java
@@ -1,17 +1,27 @@
 package org.akadia.template;
 
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
 import net.md_5.bungee.event.EventHandler;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.util.concurrent.TimeUnit;
 
-
 public class Main extends Plugin implements Listener {
+    private static Configuration configuration;
+
     @Override
     public void onEnable() {
         getLogger().info("Yay! It loads!");
@@ -23,9 +33,37 @@ public class Main extends Plugin implements Listener {
         getProxy().getScheduler().runAsync(this, () -> System.out.println("Hello from Runnable"));
         getProxy().getScheduler().schedule(this, () -> {
             for (ProxiedPlayer player : ProxyServer.getInstance().getPlayers()) {
-                player.sendMessage(new TextComponent(String.format("Currently online players count: %s", getProxy().getOnlineCount())));
+                player.sendMessage(
+                        new ComponentBuilder("Currently online players count: ")
+                                .color(ChatColor.GRAY)
+                                .append(String.valueOf(getProxy().getOnlineCount()))
+                                .color(ChatColor.AQUA)
+                                .create());
             }
         }, 0, 10, TimeUnit.SECONDS);
+
+
+        if (!getDataFolder().exists()) {
+            getDataFolder().mkdir();
+        }
+
+        File file = new File(getDataFolder(), "config.yml");
+
+        if (!file.exists()) {
+            try (InputStream in = getResourceAsStream("config.yml")) {
+                Files.copy(in, file.toPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(new File(getDataFolder(), "config.yml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(configuration.getInt("version"));
 
     }
 
